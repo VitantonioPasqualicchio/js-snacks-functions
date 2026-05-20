5b07442aab2e4c71982c4a41eba680aa3ab67e88
vitantonio99 <vitantoniopasqualicchio0@gmail.com>
2026-05-20 15:15:57 +0200

    05_bonus exercise performed

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -6,13 +6,27 @@ e buonasera se è sera (oltre le 17)
 */
 
 const name = 'Mario';
+const actualHour = parseInt(prompt ('Che ore sono? Scrivi l\'orario utilizzando questo formato: (es. 13..14..ecc..)'));
 
 
 // Dichiara la funzione qui.
 
+const salutation = (name,actualHour) => {
+    const greetingText = actualHour <= 13 ? "Buongiorno" : (actualHour <= 17 ? "Buon pomeriggio" : "Buonasera");
+    return `${greetingText} ${name}`;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const message = salutation (name,actualHour);
+console.log (message);
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
+
+
+
+/* VERSIONE ONELINER
+
+return actualHour <= 13 ? `Buongiorno ${name}` : (actualHour <= 17 ? `Buon pomeriggio ${name}` : `Buonasera ${name}`);
+
+*/
