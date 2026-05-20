969ace76bf5c81bdcaf97cd1c194e8875517165c
vitantonio99 <vitantoniopasqualicchio0@gmail.com>
2026-05-20 14:27:33 +0200

    04_conta_vocali exercise performed

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,19 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+const countVowels = (word) => {
+    const vowsels = "aeiou";
+    const charsArray = word.toLowerCase().split("");
+    return charsArray.filter(singleChar => {    
+    const isMatch = (vowsels.includes(singleChar));
+    return isMatch;
+    }
+    ).length;
+    }
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const message = countVowels (word);
+console.log ('Il numero di vocali è:',message);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
