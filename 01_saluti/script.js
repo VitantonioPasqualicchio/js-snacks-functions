d21b01e3e5d51fbb224b96bd2263a1d723aaa743
vitantonio99 <vitantoniopasqualicchio0@gmail.com>
2026-05-20 12:44:28 +0200

    01_saluti exercise performed

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -2,12 +2,12 @@
 
 const userName = 'Mario';
 
-
 // Dichiara la funzione qui.
 
+const helloName = userName => console.log(`Ciao ${userName}`);
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+helloName (userName);
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
